84d9c4efad884a5703434d90509bd1714df08086
botto54 <bryanlbottorff@gmail.com>
2020-09-07 01:57:03 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     has_many :stocks, through: :portfolio
     has_many :watchlists
     has_many :stocks, through: :watchlists
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true
+    validates :password_digest, length: { minimum: 8 }
 end
