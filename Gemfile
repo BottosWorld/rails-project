b13249c36cc72cfe1eb45016e74feddc3b8a4f3e
botto54 <bryanlbottorff@gmail.com>
2020-09-17 14:26:22 -0400

    add nested routing in portfolio and watch list resources so users can have many stocks through each and be routed properly

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'devise'
 gem 'omniauth-github', github: 'omniauth/omniauth-github'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'bootstrap', '~> 4.5.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :portfolios
-  resources :watch_lists
+  
+  resources :portfolios do
+    resources :stocks
+  end
+  
+  resources :watch_lists do
+    resources :stocks
+  end
+
   resources :stocks
+
   root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
