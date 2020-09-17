1d0af2a1978535c81023bfb3080d09e9b1534f9e
botto54 <bryanlbottorff@gmail.com>
2020-09-17 14:54:51 -0400

    add stock scopes with various value ranges

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,10 @@ class Stock < ApplicationRecord
     has_many :users, through: :portfolios
     has_many :watchlists
     has_many :users, through: :watchlists
+
+    scope :elite_stocks, -> { where(stocks.value BETWEEN '10.00' AND '99.9999') }
+    scope :legendary_stocks, -> { where(stocks.value BETWEEN '100.00' AND '9999.9999') }
+    scope :common_stocks, -> { where(stocks.value BETWEEN '1.00' AND '9.9999') }
+    scope :penny_stocks, -> { where(stocks.value BETWEEN '0.0001' AND '0.9999') }
+    scope :mythic_stocks, -> { where(stocks.value BETWEEN '10000.00' AND '9999999.9999') }
 end
